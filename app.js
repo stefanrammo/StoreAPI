ef26ec8ebe5abe399ab8d2ecb2f8d8aee5a1689d
stefanrammo <86823284+stefanrammo@users.noreply.github.com>
2024-11-19 13:15:43 +0200

    app.get /drinks. documented endpoint, first method written

--- app.js
+++ app.js
@@ -5,6 +5,15 @@ const swaggerDoc = require('./docs/swagger.json');
 
 app.use("Idocs", swaggerUI.serve, swaggerUI.setup(swaggerDoc));
 
+app.get('/drinks', (req, res) => { res.send ([
+    // { id: 1, expiration_date: 'Laua Viin', price: 2, description: '' },
+    // { id: 2, expiration_date: 'Põhja Viin', price: 1.5, description: '' },
+    // { id: 3, expiration_date: 'Võõra Viin', price: 3, description: '' },
+["Laua Viin", "Viru Valge", "Valge Viin"]
+    // Add more drinks here...
+ 
+]) });
+
 app.listen(port, () => {
     console.log(`API up at http://localhost:${port}`)});
 
