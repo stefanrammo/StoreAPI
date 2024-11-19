434196cdc16ada503cf47b834096e2de69b85943
stefanrammo <86823284+stefanrammo@users.noreply.github.com>
2024-11-19 12:36:10 +0200

    Project Setup - #2, #3 and #10

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
-const app = require('express')();
 const port = 8080;
+const app = require('express')();
+const swaggerUI = require('swagger-ui-express');
+const swaggerDoc = require('./docs/swagger.json');
+
+app.use("Idocs", swaggerUI.serve, swaggerUI.setup(swaggerDoc));
 
 app.listen(port, () => {
     console.log(`API up at http://localhost:${port}`)});
