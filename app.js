b4c898bd92bad1bbc816ca5e9f12c013dd7d37d0
stefanrammo <86823284+stefanrammo@users.noreply.github.com>
2024-11-25 14:46:04 +0200

    #1, #2 and #3 of get drink by id https://trello.com/c/480hN1Vj/5-as-an-app-developer-i-want-to-get-a-full-details-of-a-given-olem-to-show-in-my-app

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const port = 8080;
 const app = require('express')();
 const swaggerUI = require('swagger-ui-express');
-const swaggerDoc = require('./docs/swagger.json');
+const yamljs = require('yamljs');
+const swaggerDoc = yamljs.load('./docs/swagger.yaml');
 
-app.use("Idocs", swaggerUI.serve, swaggerUI.setup(swaggerDoc));
+app.use("/docs", swaggerUI.serve, swaggerUI.setup(swaggerDoc));
 
 const drinks = [{ id: 1, name: 'Laua Viin', expiration_date: null, price: 2, description: '' },
     { id: 2, name: 'Põhja Viin', expiration_date: null, price: 1.5, description: '' },
@@ -15,6 +16,12 @@ app.get('/drinks', (req, res) => { res.send
 });
 
 app.get('/drinks/:id', (req, res) => {
+    if (typeof drinks[req.params.id - 1] === 'undefined') {
+        res.status(404).send({error: 'Drink not found'})
+    };
+    if (req.params.id == null) {
+        return res.status(400).send({error: 'Invalid drink ID'})
+    };
     res.send(drinks[req.params.id - 1])});
 
 app.listen(port, () => {
