4f161952333c91633033eafdb2b80c9248a77137
stefanrammo <stefanrammo@hotmail.com>
2024-12-09 15:33:03 +0200

    Testing vue reactivity with an interactive button

--- Frontend/frontend/src/App.vue
+++ Frontend/frontend/src/App.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
+import CountInteger from './components/CountInteger.vue'
 </script>
 
 <template>
@@ -9,6 +10,7 @@ import HelloWorld from './components/HelloWorld.vue'
 
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
+      <CountInteger />
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
---
+++ Frontend/frontend/src/components/CountInteger.vue
@@ -0,0 +1,12 @@
+<script>
+export default {
+    data() { return {
+                counter: 0
+        }
+    }
+}
+</script>
+
+<template>
+    <button @click="counter++">Your mouse is now {{ counter }} more clicks towards being broken.</button>
+</template>
