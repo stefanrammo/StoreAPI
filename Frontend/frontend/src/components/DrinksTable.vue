c11a29ede45e23d42312b188af80a1dae791aa47
stefanrammo <86823284+stefanrammo@users.noreply.github.com>
2025-01-07 14:02:07 +0200

    Fix merge

--- Frontend/frontend/src/components/DrinksTable.vue
+++ Frontend/frontend/src/components/DrinksTable.vue
@@ -11,7 +11,6 @@
         </thead>
         <tbody>
             <tr v-for="item in items" :key="item.id">
-<<<<<<< HEAD
             <td>{{ item.name }}</td>
             <td class="text-center">{{ item.price }}</td>
             <td>{{ item.description || 'N/A' }}</td>
@@ -33,16 +32,6 @@
                 Edit
                 </button>
             </td>
-=======
-                <td>{{ item.name }}</td>
-                <td class="text-center">{{ item.price }}</td>
-                <td>{{ item.description || 'N/A' }}</td>
-                <td class="text-end">{{ item.expiration_date || 'N/A' }}</td>
-                <td class="row d-flex justify-content-center m-0">
-                    <button class="btn btn-primary btn-sm col-sm-5 m-1" @click="$emit('edit-item', item)">Edit</button> <!-- Add Edit button -->
-                    <button class="btn btn-danger btn-sm col-sm-5 m-1" @click="$emit('delete-item', item.id)">Delete</button>
-                </td>
->>>>>>> 7d926622d7f6f34460b96839bdd4f3f8e5ac296c
             </tr>
         </tbody>
         </table>
